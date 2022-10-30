044cc8df4d992abe4f56a9346ee184a93be186a6
FaridaMukhit <mukhitfarida@gmail.com>
2022-10-30 00:25:15 +0000

    fixed small errors

--- asg/lc.tf
+++ asg/lc.tf
@@ -2,7 +2,7 @@ resource "aws_launch_configuration" "main" {
   name            = "${var.env}-main-lc-new"
   image_id        = "ami-06640050dc3f556bb"
   instance_type   = var.instance_type
-  user_data       = data.template_file.userdata.rendered
+  user_data       = data.template_file.user_data.rendered
   security_groups = [aws_security_group.main.id]
   key_name        = aws_key_pair.terrafrom-server-key.key_name
 
